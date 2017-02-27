e9a503a344f72341e5245111092d43bb2958888f
jamie <jamie.buckley@quantarc.co.uk>
2017-02-27 23:26:26 +0000

    Added support for sass theme development into webpack

--- src/ts/main.tsx
+++ src/ts/main.tsx
@@ -5,6 +5,15 @@ import { Provider } from 'react-redux'
 import todoApp  from './reducers/reducers'
 import App from './components/App'
 
+
+declare var require: {
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+};
+require('../scss/theme.scss');
+
+
 let store = createStore(todoApp);
 console.log(store.getState())
 let unsubscribe = store.subscribe(() =>
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            { test: /\.js$/, loader: "source-map-loader", enforce: "pre" }
+            { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+            { test: /\.scss$/, loader: [ 'style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap']}
         ]
     },
     devServer: { inline: true }
